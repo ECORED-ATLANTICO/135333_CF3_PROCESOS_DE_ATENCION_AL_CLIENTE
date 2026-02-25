42cf6ee7be73055236b4eab3fe6ddde39597ddaa
Fabio Fonseca <fabiofonseca.arg@gmail.com>
2026-02-25 11:22:36 -0500

    tema 5

--- src/views/Tema5.vue
+++ src/views/Tema5.vue
@@ -5,7 +5,180 @@
     .titulo-principal.color-acento-contenido
       .titulo-principal__numero
         span 5
-      h1 Tema 5
+      h1 Objeciones
+    
+    .row.mb-4.justify-content-center.align-items-center
+      .col-12.col-md-8.col-lg-4(data-aos="zoom-in-right")
+        figure
+            img.mb-4.mb-lg-0(src='@/assets/curso/tema5/1.png', alt='Texto que describa la imagen')
+      .col-12.col-lg-8(data-aos="zoom-in-left")
+        p Surgen dentro de la recepción de requerimientos, cuando el cliente siente que el agente de servicio no está entendiendo el requerimiento o cuando el agente le indica que no tiene alcance para recepcionarlo; por tanto, para un agente de servicio es muy importante conocer sobre objeciones, dado que al interactuar con el cliente, este probablemente manifieste no entender lo que se le está explicando, tener dudas o incluso, requerir más información. En palabras más sencillas, las objeciones son una serie de preguntas y afirmaciones interpuestas por los clientes cuando no siente empatía por el personal de la empresa hacia su solicitud.
+        p.mb-0 Existen muchas interpretaciones de lo que es una objeción en términos de servicio al cliente. Algunos autores consideran que hace parte del proceso normal de interacción entre un cliente y un representante de la empresa; sin embargo, hay otros que indican que la presencia de objeciones puede denotar desconocimiento por parte de alguno de los dos interlocutores (agente de servicio o cliente). No obstante, a pesar de no existir consenso en por qué se dan, sí es claro, que es un tema clave que un agente de servicio al cliente debe conocer.
+    
+    p.mb-5 En consecuencia, a continuación, se enuncian dos definiciones de objeciones de autores y una propia:
+
+    .bg-franja.bg-franja--fullheight.mb-5.p-2.p-sm-4
+      .row.align-items-center.justify-content-center
+        .col-12.col-md-8.col-lg-3
+          figure
+            img(src='@/assets/curso/tema5/2.png', alt='Texto que describa la imagen')
+        .col-12.col-lg-9.align-items-center.p-3
+          div
+            ol.lista-ol--cuadro.mb-0
+              li.d-flex 
+                .lista-ol--cuadro__vineta.bg-terciario
+                  span 
+                | La Real Academia de la Lengua Española RAE (2021) referencia que la objeción es la “razón que se propone o dificultad que se presenta en contra de una opinión o designio, o para impugnar una proposición”.
+              li.d-flex 
+                .lista-ol--cuadro__vineta.bg-terciario
+                  span 
+                | Ardila (1984) “las objeciones son cualquier acto del comprador que de forma real o aparente interrumpe la labor del vendedor” (p. 9). 
+              li.d-flex.mb-0 
+                .lista-ol--cuadro__vineta.bg-terciario
+                  span 
+                | En sentido práctico, son los “peros” que expone el cliente potencial al vendedor durante el proceso de la venta.
+
+    .row.justify-content-center.mb-4
+      .col-xl-10
+        h4(data-aos='zoom-in-right')
+          span.etiqueta-title Tipos de objeciones
+
+    .row.justify-content-center.mb-4.mb-lg-5
+      .col-xl-10
+        p.mb-4 Las objeciones desde la perspectiva de servicio, al igual que la mayoría de temas vistos, no tienen una única orientación; sin embargo, hay consenso en que existen a nivel general, dos tipos, los cuales se describen a continuación:
+
+    div.slider-wrapper.z-2.mb-6
+      .bg-fullwidth
+      .tarjeta.tarjeta--blanca.position-relative.p-4.pt-5.z-2.my-5
+        SlyderA(tipo="b")
+          .row.z-2.justify-content-center.align-items-center
+            .col-lg-5
+              h5 Objeciones verdaderas
+              p Se presentan cuando el cliente está realmente interesado en el tema a tratar; sin embargo, manifiesta inquietudes o dudas respecto a la información que se le está suministrando y si las respuestas realmente satisfacen las necesidades que él tiene, por lo que busca canalizarlas a través del agente de servicio.
+              p.mb-4.mb-lg-0 #[strong Ejemplo:] un cliente es contactado por un agente de servicio con el fin de conocer si el cliente está interesado en programar un mantenimiento preventivo para su lavadora, y la persona pregunta ¿por qué tendría que realizarlo si su electrodoméstico se encuentra en buen estado?, esta es una objeción verdadera porque obedece a un argumento racional, donde al obtener la respuesta podría tener un escenario más amplio para tomar la decisión.
+            .col-lg-6
+              figure
+                img(src='@/assets/curso/tema5/3.png', alt='Imagen de acompañamiento')
+          .row.z-2.justify-content-center.align-items-center
+            .col-lg-5
+              h5 Objeciones falsas
+              p Se dan cuando el cliente no está interesado en la información o en el producto, y por ende basa la comunicación en pretextos o cuestionamientos sin racionalidad, con el fin de dispersar la conversación o no manifestar la razón real por la que no desea adquirir los productos de la empresa o no desea tener relación comercial con ella.
+              p.mb-4.mb-lg-0 #[strong Ejemplo:] un cliente es contactado por un agente de servicio quien le ofrece un bono de descuento a un nuevo restaurante, el cual tiene 6 meses para ser usado, el cliente informa que no está interesado porque tiene mucho trabajo y no tiene tiempo para asistir a dicho restaurante, esto claramente es una objeción falsa porque aun cuando lo que manifiesta el cliente sea cierto, el tiempo para el disfrute del bono es bastante amplio, por lo que definitivamente este no es un argumento sólido para no aceptar el beneficio.
+            .col-lg-6
+              figure
+                img(src='@/assets/curso/tema5/4.png', alt='Imagen de acompañamiento')
+
+    .fullwidth-bg.fullwidth-bg--1
+    .row.justify-content-center.mb-4.mb-lg-5
+      .col-lg-10.col-xl-8
+        p.mb-0 Asociado al manejo de objeciones, es claro mencionar que, si bien existe la teoría, las clasificaciones y los ejemplos, hay dos factores que influyen en la gestión de las mismas: 
+        
+    .row.justify-content-center.mb-4.mb-lg-5
+      .col-md-5.col-xl-3.mb-4.mb-xl-0(data-aos="fade-up")
+        .tarjeta-numerada.color-primario.ps-5.px-4.py-3.h-100.d-flex.align-items-center
+          .tarjeta-numerada__numero
+            .h2 1
+          P.mb-0 La personalidad del agente, dado que la esencia de la persona influye mucho en el trato con las demás personas.
+      .col-md-7.col-xl-5.mb-4.mb-xl-0(data-aos="fade-up")
+        .tarjeta-numerada.color-primario.ps-5.px-4.py-3.h-100.d-flex.align-items-center
+          .tarjeta-numerada__numero
+            .h2 2
+          P.mb-0 La experiencia, cada vez que se tiene mayor contacto con los clientes se van adquiriendo más destrezas para interactuar y manejar situaciones que a simple vista incluso pueden ser inmanejables.
+
+    .row.justify-content-center.mb-4
+      .col-xl-10
+        h4(data-aos='zoom-in-right')
+          span.etiqueta-title Resolución de objeciones
+
+    .row.justify-content-center.mb-4.mb-lg-5
+      .col-xl-10
+        p.mb-0 Navarro (2012) en su libro Técnicas de ventas propone un esquema interesante para el manejo de objeciones, comenzando por la escucha como el principio básico de la comunicación, luego a partir de la asertividad anticiparse a posibles alternativas de respuesta o solución, posteriormente a través de un ejercicio de empatía comprender la situación y finalmente si el cliente aún tiene inquietudes y refuta lo expuesto, exponer con argumentos claros y respetuosos la alternativa de solución.
+
+    .row.justify-content-center
+      .col-12.col-lg-8.col-xl-6
+        PasosA.color-primario.mb-5(tipo="l")(data-aos="zoom-in")
+          .row.align-items-center
+            .col-4.col-md-3.col-lg-2
+              figure
+                img.mb-4.mb-lg-0(src='@/assets/curso/tema5/11.svg', alt='Texto que describa la imagen')
+            .col-md-8.col-lg-9.align-items-center
+              div  
+                h3 Escuchar
+                p.mb-0 No emitir juicios apresurados y prestar completa atención a lo que dice el cliente.
+          .row.align-items-center
+            .col-md-8.col-lg-9.align-items-center
+              div  
+                h3.text-start Anticiparse
+                p.text-start.mb-4.mb-lg-0 Se puede ganar tiempo, si se anticipan las posibles objeciones.
+            .col-4.col-md-3.col-lg-2
+              figure
+                img(src='@/assets/curso/tema5/12.svg', alt='Texto que describa la imagen')
+          .row.align-items-center
+            .col-4.col-md-3.col-lg-2
+              figure
+                img.mb-4.mb-lg-0(src='@/assets/curso/tema5/13.svg', alt='Texto que describa la imagen')
+            .col-md-8.col-lg-9
+              div  
+                h3 Comprender
+                p.mb-0 Si el cliente percibe empatía por parte del vendedor, sentirá confianza para continuar el proceso de negociación.
+          .row.align-items-center
+            .col-md-8.col-lg-9
+              div  
+                h3.text-start Refutar
+                p.text-start Respeto, paciencia, inteligencia y argumentos son clave para responder objeciones.
+            .col-4.col-md-3.col-lg-2
+              figure
+                img(src='@/assets/curso/tema5/14.svg', alt='Texto que describa la imagen')
+
+    .row.justify-content-center.mb-4.mb-lg-5
+      .col-xl-10
+        p.mb-0 A nivel general, las objeciones más frecuentes que los clientes manifiestan a los agentes de servicios están relacionadas con:
+
+    TabsB.color-secundario.mb-5(data-aos="fade-left")
+      .p-3.p-lg-5(titulo="Precio")
+        .row.align-items-center.justify-content-evenly
+          .col-lg-8.col-xl-7
+            p.mb-4.mb-lg-0 Cuando los agentes tienen también rol comercial, el precio será siempre la variable de objeción por excelencia, dado que los clientes siempre querrán pagar menos para obtener los bienes y servicios de la empresa. En algunos casos, la cultura también impacta este tipo de objeciones, lo que comúnmente se llama “regatear”. El manejo de esta objeción se da principalmente en contrarrestar con argumentos desde las características, beneficios y ventajas del producto, así se tratará de mostrar una relación costo - beneficio positiva; en otras ocasiones, cuando el agente tiene el perfil de negociar también puede acudir a los descuentos para tratar de cerrar la venta.
+          .col-8.col-md-6.col-lg-4.col-xl-3
+            figure
+              img.img-140(src='@/assets/curso/tema5/5.svg', alt='Texto que describa la imagen')
+      .p-3.p-lg-5(titulo="Comparación con la competencia")
+        .row.align-items-center.justify-content-evenly
+          .col-lg-8.col-xl-7
+            p Es frecuente que los clientes acudan a comparar a la empresa con los competidores, indicando que ellos hacen las cosas mejor, para ello el mecanismo de argumento frente a esto es conocer muy bien la empresa y su oferta de valor (su diferencial en el mercado), con la finalidad de hacer visible porque la compañía también tiene muchos puntos a favor.
+          .col-8.col-md-6.col-lg-4.col-xl-3
+            figure
+              img.img-140(src='@/assets/curso/tema5/6.svg', alt='Texto que describa la imagen')
+      .p-3.p-lg-5(titulo="Tiempos de entrega")
+        .row.align-items-center.justify-content-evenly
+          .col-lg-8.col-xl-7
+            p Cuando el agente de servicio tiene alcance en el proceso de gestión de pedidos, los tiempos de entrega son una variable crítica para los clientes, y frecuentemente se contactan con la empresa para obtener información sobre el estado del pedido, para ello el primer paso del agente del servicio es validar si dicho pedido aún se encuentra dentro de los tiempos de entrega o ya está retrasado, en el primer caso, se debe hacer visible al cliente que el pedido se encuentra en gestión y que en cualquier momento puede llegar a sus manos. En el segundo caso, cuando ya los tiempos han pasado el agente de servicio debe escalar el caso y entregarle al cliente (en la medida de lo posible) una fecha u hora estimada de entrega.
+          .col-8.col-md-6.col-lg-4.col-xl-3
+            figure
+              img.img-140(src='@/assets/curso/tema5/7.svg', alt='Texto que describa la imagen')
+      .p-3.p-lg-5(titulo="Inquietud sobre el porqué del contacto")
+        .row.align-items-center.justify-content-evenly
+          .col-lg-8.col-xl-7
+            p Es frecuente que los clientes acudan a comparar a la empresa con los competidores, indicando que ellos hacen las cosas mejor, para ello el mecanismo de argumento frente a esto es conocer muy bien la empresa y su oferta de valor (su diferencial en el mercado), con la finalidad de hacer visible porque la compañía también tiene muchos puntos a favor.
+          .col-8.col-md-6.col-lg-4.col-xl-3
+            figure
+              img.img-140(src='@/assets/curso/tema5/8.svg', alt='Texto que describa la imagen')
+      .p-3.p-lg-5(titulo="Agente NO resuelve requerimiento del cliente")
+        .row.align-items-center.justify-content-evenly
+          .col-lg-8.col-xl-7
+            p Desde la óptica de cliente, siempre se espera que en el primer contacto los requerimientos quedan resueltos; sin embargo, por temas de perfiles, estructura organizacional y procedimientos técnicos, no siempre será así. Por tanto, el rol del agente de servicio será tener una comunicación efectiva con el cliente, informando en detalle por qué no puede gestionar su requerimiento, mostrando interés en la gestión del mismo, aunque aclarando que deben respetarse los flujos, tiempos o lineamientos de la empresa.
+          .col-8.col-md-6.col-lg-4.col-xl-3
+            figure
+              img.img-140(src='@/assets/curso/tema5/9.svg', alt='Texto que describa la imagen')
+
+
+
+
+
+
+
+
+
 
 
 </template>
